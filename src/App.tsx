0bb021d70ed11c29f3110e0c7a89c48f75576ea2
alxjrvs@gmail.com <alxjrvs@gmail.com>
2025-10-19 01:04:47 -0400

    No base

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ function App() {
   }
 
   return (
-    <Router basename="/SUIndex/">
+    <Router basename="/">
       <div className="flex h-screen bg-[var(--color-su-white)]">
         <Navigation schemas={schemaIndex.schemas} />
         <main className="flex-1 overflow-auto">
