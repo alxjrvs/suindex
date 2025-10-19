9ef33762a254ef6587ccf76706b088171931cb0e
alxjrvs@gmail.com <alxjrvs@gmail.com>
2025-10-19 01:42:46 -0400

    tweak colors

--- src/components/specialized/shared/DataList.tsx
+++ src/components/specialized/shared/DataList.tsx
@@ -26,14 +26,52 @@ export function DataList({
               •
             </span>
           )}
-          <span
-            style={{ color: textColor }}
-            className={`${item.cost ? "font-bold" : ""} ${
-              invert ? "opacity-90" : ""
-            }`}
-          >
-            {item.value}
-          </span>
+          {item.cost ? (
+            <span
+              className="inline-flex items-center"
+              style={{ overflow: "visible" }}
+            >
+              {/* Black badge with white text */}
+              <span
+                style={{
+                  backgroundColor: "var(--color-su-black)",
+                  color: "var(--color-su-white)",
+                  fontSize: "12px",
+                  paddingLeft: "4px",
+                  paddingRight: "4px",
+                  paddingTop: "2px",
+                  paddingBottom: "2px",
+                  height: "16px",
+                  fontWeight: "bold",
+                  zIndex: 2,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                {item.value}
+              </span>
+              {/* Triangle arrow pointing right */}
+              <div
+                style={{
+                  width: 0,
+                  height: 0,
+                  borderTop: "8px solid transparent",
+                  borderBottom: "8px solid transparent",
+                  borderLeft: "8px solid var(--color-su-black)",
+                  marginLeft: "0px",
+                  zIndex: 1,
+                }}
+              />
+            </span>
+          ) : (
+            <span
+              style={{ color: textColor }}
+              className={`${invert ? "opacity-90" : ""}`}
+            >
+              {item.value}
+            </span>
+          )}
         </span>
       ))}
     </span>
