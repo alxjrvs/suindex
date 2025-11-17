90be56fc4b211d224a5d7b96bd12cee8c4e7c78b
alxjrvs@gmail.com <alxjrvs@gmail.com>
2025-11-17 18:02:50 -0500

    Cleanup

--- apps/suref-web/.vite/deps/react-dom.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-NZYFM45U.js";
-import "./chunk-TZPOCJSC.js";
-import "./chunk-V4OQ3NZ2.js";
-export default require_react_dom();
--- apps/suref-web/.vite/deps/react_jsx-runtime.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_jsx_runtime
-} from "./chunk-X3PCUOMJ.js";
-import "./chunk-TZPOCJSC.js";
-import "./chunk-V4OQ3NZ2.js";
-export default require_jsx_runtime();
